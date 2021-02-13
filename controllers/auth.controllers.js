9aff64cd9304c94b8b88ca621a721036d561a2e1
Nilanjan Deb <nilanjan172nsvian@gmail.com>
2021-02-14 02:44:00 +0530

    add user sign up service

--- controllers/auth.controllers.js
+++ controllers/auth.controllers.js
@@ -1,7 +1,15 @@
-const bcrypt = require('bcrypt');
 const asyncHandler = require('../middleware/asyncHandler');
 const ErrorResponse = require('../utils/errorResponse');
 const checker = require('../utils/checkFields');
+const { signUp } = require('../services/auth.services');
+
+/**
+ *
+ * @desc User Sign Up Handler
+ * @route /api/v1/auth/register
+ * @access public
+ *
+ */
 
 const registerHandler = asyncHandler((req, res, next) => {
 	let { name, email, password } = req.body;
@@ -25,66 +33,8 @@ const registerHandler = asyncHandler((req, res, next) => {
 		throw new ErrorResponse('Please provide a strong password', 400);
 	}
 
-	// Hashing Password
-	bcrypt.hash(
-		password,
-		parseInt(process.env.BCRYPT_SALT_LENGTH),
-		(err, hash) => {
-			if (err) return next(err);
-
-			// Creating SQL Query to insert Data
-			let query = `
-				INSERT INTO USERS(NAME, EMAIL_ADDRESS, PASSWORD)
-					VALUES(
-						"${name}",
-						"${email}",
-						"${hash}"
-					);
-				`;
-
-			// Apply query to SQL
-			db.query(query, (err) => {
-				// Database Error Handling
-				if (err) {
-					if (err.code === 'ER_DUP_ENTRY')
-						next(new ErrorResponse('Email address already in use!', 403));
-					else next(err);
-					return;
-				}
-
-				// Find Number of users with same name
-				query = `
-					SELECT COUNT(ID) AS num FROM USERS WHERE NAME LIKE '${name}';
-				`;
-				db.query(query, (err, result) => {
-					if (err) return next(err);
-
-					// Unique Username create (name.number)
-					let username = name.replace(/\s/g, '.').toLowerCase();
-					if (result[0].num > 1) username += result[0].num;
-
-					// Update User with unique username
-					query = `
-						UPDATE USERS SET USERNAME = "${username}" WHERE EMAIL_ADDRESS = "${email}";
-					`;
-					db.query(query, (err) => {
-						if (err) return next(err);
-
-						// Successful Response
-						return res.status(201).json({
-							success: true,
-							message:
-								'User registration successful. Kindly verify your email address',
-							responses: {
-								accessToken: null,
-								emailVerified: false,
-							},
-						});
-					});
-				});
-			});
-		}
-	);
+	// Call Sign Up Service for create user account
+	signUp({ name, email, password }, res, next);
 });
 
 const loginHandler = (req, res, next) => {};
--- services/auth.services.js
+++ services/auth.services.js
@@ -1,14 +1,14 @@
 const bcrypt = require('bcrypt');
 const ErrorResponse = require('../utils/errorResponse');
 
-const signUp = async (data) => {
+const signUp = (data, res, next) => {
 	let { name, email, password } = data;
 	// Hashing Password
-	return bcrypt.hash(
+	bcrypt.hash(
 		password,
 		parseInt(process.env.BCRYPT_SALT_LENGTH),
 		(err, hash) => {
-			if (err) throw err;
+			if (err) return next(err);
 
 			// Creating SQL Query to insert Data
 			let query = `
@@ -21,42 +21,43 @@ const signUp = async (data) => {
 				`;
 
 			// Apply query to SQL
-			return db.query(query, (err) => {
+			db.query(query, (err) => {
 				// Database Error Handling
 				if (err) {
 					if (err.code === 'ER_DUP_ENTRY')
-						return new ErrorResponse('Email address already in use!', 403);
-					else throw err;
+						next(new ErrorResponse('Email address already in use!', 403));
+					else next(err);
+					return;
 				}
 
 				// Find Number of users with same name
 				query = `
 					SELECT COUNT(ID) AS num FROM USERS WHERE NAME LIKE '${name}';
 				`;
-				return db.query(query, (err, result) => {
-					if (err) throw err;
+				db.query(query, (err, result) => {
+					if (err) return next(err);
 
 					// Unique Username create (name.number)
-					let username = name.replace(/\s/g, '.');
+					let username = name.replace(/\s/g, '.').toLowerCase();
 					if (result[0].num > 1) username += result[0].num;
 
 					// Update User with unique username
 					query = `
 						UPDATE USERS SET USERNAME = "${username}" WHERE EMAIL_ADDRESS = "${email}";
 					`;
-					return db.query(query, (err, result) => {
-						if (err) throw err;
+					db.query(query, (err) => {
+						if (err) return next(err);
 
 						// Successful Response
-						return {
+						res.status(201).json({
 							success: true,
 							message:
 								'User registration successful. Kindly verify your email address',
-							result: {
+							responses: {
 								accessToken: null,
 								emailVerified: false,
 							},
-						};
+						});
 					});
 				});
 			});
