3900604b258a0fbf35e6cdc02880ab628fc42ba8
Nilanjan Deb <nilanjan172nsvian@gmail.com>
2021-04-09 03:42:49 +0530

    server deployment

--- index.js
+++ index.js
@@ -55,6 +55,15 @@ const server = app.listen(PORT, () => {
 	console.log(`Server started on port ${PORT}`);
 });
 
+// serve static files
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static(__dirname, 'frontend/build'));
+
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+	});
+}
+
 // Handle unhandled Promise rejections
 process.on('unhandledRejection', (err) => {
 	console.log(`Error: ${err.message}`);
